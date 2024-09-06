d2dbeef9c73fe85d2449e72332186037cfd5befb
Ito Jun <jun.ito@kiramex.com>
2024-09-07 07:01:52 +0900

    yet

---
+++ test/components/AppAlert.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="status === 'pending'">
+      Loading...
+    </div>
+    <div v-else>
+      <div v-for="post in posts">
+        <h2>{{ post.id }}{{ post.location }}</h2>
+      </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const { status, data: posts } = await useFetch("https://api.sampleapis.com/wines/reds", {
+  //lazy: true,
+  server: false
+});
+
+debugger
+</script>
